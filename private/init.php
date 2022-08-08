ec6ef44fe21b5c5dc5dd7d2a86f3e95c27652855
HypeByte <HypeByteProgrammer@gmail.com>
2022-08-07 19:13:29 -0700

    remove redirect

--- private/init.php
+++ private/init.php
@@ -1,5 +1,4 @@
 <?php
-header('location: https://erhscodingclub.org/index.php');
 define("PRIVATE_PATH", dirname(__FILE__));
 define("PROJECT_PATH", dirname(PRIVATE_PATH));
 define("PUBLIC_PATH", PROJECT_PATH . '/public');
