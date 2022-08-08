606de71516f2e7b4b37046f479a8fbc818ea2f2f
HypeByte <HypeByteProgrammer@gmail.com>
2022-08-07 17:43:49 -0700

    Updated meta information for website

--- private/temp/header.php
+++ private/temp/header.php
@@ -3,6 +3,10 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="description" content="A club for highs school students to learn real coding for free.">
+    <meta name="keywords" content="club education school coding programming free python html css js volunteer hours">
+    <meta name="author" content="Ayush Moghe">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title><?php echo $page_name; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
