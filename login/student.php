5f5c3d0f950847e3c92a571c9c69e0d2cbc5f3c8
HypeByte <HypeByteProgrammer@gmail.com>
2022-08-13 22:22:39 -0700

    Updated login for better format

---
+++ login/student.php
@@ -0,0 +1,67 @@
+<?php
+$page_style = "webpage";
+$page_name = "Student Login";
+?>
+
+<?php require_once "../private/init.php"; ?>
+<?php require_once "../private/temp/headerNest.php"; ?>
+<?php require_once "../private/temp/navbarNest.php"; ?>
+
+<div class="container-fluid text-centet">
+    <form method="post" action="login.php" class="w-75 ps-5 pe-5" style="margin: auto; background-color: rgba(91, 232, 105, 0.63); border-radius: 15px;">
+
+        <div class="h1 fs-sm-5 erhs-h1 text-center mb-4 mt-3" style="font-family: 'Orbitron', sans-serif; text-shadow: 2px 2px 2px white;">Login/ Register</div>
+
+        <hr style="opacity: 1; color: white; height: 5px;">
+
+        <ul class="nav nav-pills justify-content-center mt-4">
+            <li class="nav-item me-2 ms-2">
+                <a class="nav-link bg-dark pe-5 ps-5 pt-3 pb-3" aria-current="page" href="register.php">Register</a>
+            </li>
+            <li class="nav-item dropdown me-2 ms-2">
+                <a class="nav-link dropdown-toggle bg-dark pe-5 ps-5 pt-3 pb-3" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Login</a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="student.php">Login as Student</a></li>
+                    <li><a class="dropdown-item" href="teacher.php">Login as Teacher</a></li>
+                </ul>
+            </li>
+        </ul>
+        <hr style="opacity: 1; color: red; height: 5px;">
+
+        <div class="h2 erhs-h2 text-center mb-3 mt-3" style="font-family: 'Orbitron', sans-serif;">Student Login</div>
+
+        <div>
+            <label class="form-label mt-2 text-light" for="username">Student ID</label>
+            <div class="input-group input-group-lg">
+                <span class="input-group-text" id="inputGroup-sizing-lg"><i class="bi bi-credit-card-2-front"></i></span>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Enter id" style="color: white; background-color: orange;">
+            </div>
+        </div>
+
+        <div>
+            <label class="form-label mt-2 text-light" for="username">Username</label>
+            <div class="input-group input-group-lg">
+                <span class="input-group-text" id="inputGroup-sizing-lg"><i class="bi bi-person-circle"></i></span>
+                <input type="text" class="form-control form-control-lg" id="username" name="username" placeholder="Enter username" style="color: white; background-color: orange;">
+            </div>
+
+        </div>
+
+        <div>
+            <label class="form-label mt-2 text-light" for="password">Password</label>
+            <div class="input-group input-group-lg">
+                <span class="input-group-text" id="inputGroup-sizing-lg"><i class="bi bi-asterisk"></i></span>
+                <input type="password" class="form-control form-control-lg" id="password" name="password" placeholder="Enter password" style="color: white; background-color: orange;">
+            </div>
+
+        </div>
+
+        <button type="submit" class="btn btn-primary mt-3 mb-3">Login</button>
+    </form>
+
+</div>
+
+
+
+<?php require_once "../private/temp/footer.php"; ?>
+
--- private/temp/navbarNest.php
+++ private/temp/navbarNest.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-flex navbar-dark navbar-expand-md bg-dark py-3 sticky-top" style="">
-    <div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><img src="./private/assets/img/ERHSCC.png" width="104" height="87"></span><span><strong class="erhs-h1">&nbsp; ERHS Coding Club</strong></span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-5"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><img src="../private/assets/img/ERHSCC.png" width="104" height="87"></span><span><strong class="erhs-h1">&nbsp; ERHS Coding Club</strong></span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-5"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-5">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link active" href="../index.php"><b>Home</b></a></li>
                 <li class="nav-item"><a class="nav-link active" href="#">Dashboard</a></li>
-                <li class="nav-item"><a class="nav-link" href="about.php">About us</a></li>
+                <li class="nav-item"><a class="nav-link" href="../about.php">About us</a></li>
                 <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Account</a>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="#">Profile</a>
@@ -12,7 +12,7 @@
                         <a class="dropdown-item" href="#">Settings</a>
                     </div>
                 </li>
-            </ul><a class="btn btn-primary ms-md-2" role="button" href="login/student.php">Login/Register</a>
+            </ul><a class="btn btn-primary ms-md-2" role="button" href="../login/student.php">Login/Register</a>
         </div>
     </div>
 </nav>
