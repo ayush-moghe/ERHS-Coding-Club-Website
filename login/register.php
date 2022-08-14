0aae9acbca881cea071bf228bbf0f87a1385155c
HypeByte <HypeByteProgrammer@gmail.com>
2022-08-14 13:59:03 -0700

    added composer

--- login/register.php
+++ login/register.php
@@ -1,12 +1,11 @@
 <?php
-require_once "../private/init.php";
 $page_style = "webpage";
 $page_name = "Student Login";
 $inline = "background-image: url('../private/assets/img/ERHSCC.png'); background-repeat: no-repeat; background-position: center; background-size: cover;";
 
 ?>
 
-
+<?php require_once "../private/init.php"; ?>
 <?php require_once "../private/temp/headerNest.php"; ?>
 <?php require_once "../private/temp/navbarNest.php"; ?>
 
