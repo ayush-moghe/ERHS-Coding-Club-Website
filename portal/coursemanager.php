4b6d5f8419b4aed885d51b4325d001e1e5d21de6
HypeByte <HypeByteProgrammer@gmail.com>
2023-01-10 00:07:04 -0800

    debugging

--- portal/coursemanager.php
+++ portal/coursemanager.php
@@ -1,82 +1,53 @@
- <?php
- $page_style = 'portal';
- $page_name = 'Course Manager';
- require_once "../private/init.php";
- $ercc_db = db_connect('../private/cert/BaltimoreCyberTrustRoot.crt.pem');
- require_login('../login/login.php');
- require_role( STAFFROLES, $ercc_db);
+<?php
+$page_name = 'My Portal';
+$page_style = 'portal';
+require_once "../private/init.php";
+$ercc_db = db_connect('../private/cert/BaltimoreCyberTrustRoot.crt.pem');
+require_login('../login/login.php');
+require_role(STAFFROLES, $ercc_db);
+$roles = explode( ',' , user_roles($_SESSION['username'], $ercc_db) );
+?>
+
+<?php require_once "../private/temp/headerNest.php"; ?>
+<?php require_once "../private/temp/navbarNest.php"; ?>
 
 
+    <div id="wrapper">
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand fs-3 bg-light" style="color: black"><b>MY PORTAL</b></li>
+                <li class="fs-4 mt-4" style="color: gold"><b>Services ⚙️</b></li>
+                <div class="dropdown-divider" style="border-color: white;"></div>
+                <li class="fs-5 mt-2 text-light"><a style="background-color: white; color: black;" href="index.php">Home</a></li>
+                <li class="fs-5 mt-2 text-light"><a href="coursemaker.php">Course Maker</a></li>
+                <li class="fs-5 mt-2 text-light"><a href="coursemanager.php">Course Manager</a></li>
+                <li class="fs-5 mt-2 text-light"><a href="users.php">User Table</a></li>
+                <li class="fs-5 mt-2 text-light"><a href="verify.php">Verification Hub</a></li>
+            </ul>
+        </div>
+        <div class="page-content-wrapper">
+            <div class="container-fluid"><a id="menu-toggle" class="btn btn-link" role="button" href="#menu-toggle"><i class="bi bi-gear-fill fs-1"></i></a>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="h1 text-center text-light mt-2" style="font-family: 'Montserrat', sans-serif;">Welcome, <?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name'] ?></div>
+                        <div class="container container-flex text-center mt-5" style="border-radius: 10px; border: 4px solid black;">
+                            <div class="h2 text-info mt-2" style="font-family: 'Orbitron', sans-serif;">My Roles:</div>
+                            <?php
+                            foreach($roles as $role) {
+                                display_role($role);
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
- ?>
-
- <?php require_once "../private/temp/headerNest.php"; ?>
- <?php require_once "../private/temp/navbarNest.php"; ?>
-
-
- <div id="wrapper">
-     <div id="sidebar-wrapper">
-         <ul class="sidebar-nav">
-             <li class="sidebar-brand fs-3 bg-light" style="color: black"><b>MY PORTAL</b></li>
-             <li class="fs-4 mt-4" style="color: gold"><b>Services ⚙️</b></li>
-             <div class="dropdown-divider" style="border-color: white;"></div>
-             <li class="fs-5 mt-2 text-light"><a href="index.php">Home</a></li>
-             <li class="fs-5 mt-2 text-light"><a href="coursemaker.php">Course Maker</a></li>
-             <li class="fs-5 mt-2 text-light"><a style="background-color: white; color: black;" href="coursemanager.php">Course Manager</a></li>
-             <li class="fs-5 mt-2 text-light"><a href="users.php">User Table</a></li>
-             <li class="fs-5 mt-2 text-light"><a href="verify.php">Verification Hub</a></li>
-         </ul>
-     </div>
-     <div class="page-content-wrapper">
-         <div class="container-fluid"><a id="menu-toggle" class="btn btn-link" role="button" href="#menu-toggle"><i class="bi bi-gear-fill fs-1"></i></a>
-             <h1 class="text-center fs-1" style="color: lightblue">Choose A Course</h1>
-             <form method="post" action="coursemanager.php">
-                 <div class="row w-75 mt-5" style="margin: auto;">
-                     <?php while($course = mysqli_fetch_assoc($courses)) { ?>
-                         <div class="col mt-4">
-                             <div class="card h-100" style="background-color: dimgrey; border: 4px solid darkgreen;">
-                                 <img class="card-img-top" src="../private/assets/img/ERHSCC.png" alt="python logo">
-                                 <div class="card-header" style="border-bottom: 2px solid white;">
-                                     <h4 class="card-title" style="color: orange"><b><?php echo $course['course_name']; ?></b></h4>
-                                     <h5 style="color: yellow"><i>Author: <?php echo $course['author']; ?></i></h5>
-                                     <h5>Approved:
-                                         <?php if (check_course_approved($ercc_db, $course['id'])) { ?>
-                                             <i class="bi bi-check-circle-fill text-success"></i>
-                                         <?php } else { ?>
-                                             <i class="bi bi-x-circle-fill text-danger"></i>
-                                         <?php } ?>
-                                     </h5>
-                                 </div>
-                                 <div class="card-body">
-                                     <p class="card-text" style="color: white;"><b style="color: lawngreen">Description: </b> <?php echo $course['description']; ?></p>
-                                 </div>
-                                 <div class="card-footer">
-                                     <button class="btn btn-primary mt-1" type="submit" name="course" value="<?php echo $course['id']; ?>">Build/Edit</button>
-                                     <button class="btn btn-success mt-1" type="submit" name="preview" value="<?php echo $course['id']; ?>">Preview</button>
-                                     <?php if (check_if_role($ercc_db, 'admin,teacher') ) { ?>
-                                         <?php if (!check_course_approved($ercc_db, $course['id']) ) { ?>
-                                            <button class="btn btn-light mt-1" type="submit" name="approve" value="<?php echo $course['id']; ?>">Approve</button>
-                                         <?php } else { ?>
-                                            <button class="btn btn-warning mt-1" type="submit" name="disapprove" value="<?php echo $course['id']; ?>">Disapprove</button>
-                                         <?php } ?>
-                                     <?php } ?>
-                                     <?php if (check_if_role($ercc_db, 'admin') ) { ?>
-                                        <button class="btn btn-danger mt-1" type=submit" name="delete" value="<?php echo $course['id']; ?>">Delete</button>
-                                     <?php } ?>
-                                 </div>
-                             </div>
-                         </div>
-                     <?php } ?>
-                 </div>
-             </form>
-         </div>
-     </div>
- </div>
-
- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
- <script src="../private/assets/js/Sidebar-Menu.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="../private/assets/js/Sidebar-Menu.js"></script>
 
 
 
 
- <?php require_once "../private/temp/footer.php"; ?>
+<?php require_once "../private/temp/footer.php"; ?>
