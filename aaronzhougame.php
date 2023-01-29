c382f0937651c10671bd77054c05699da80bedba
HypeByte <HypeByteProgrammer@gmail.com>
2023-01-29 14:55:08 -0800

    game fixed course player

---
+++ aaronzhougame.php
@@ -0,0 +1,31 @@
+<?php
+$page_style = 'webpage';
+$page_name = 'Aaron Zhou Game';
+require_once "./private/init.php";
+?>
+
+<?php require_once './private/temp/header.php'; ?>
+<?php require_once './private/temp/navbar.php'; ?>
+
+
+<div class="container-fluid text-center">
+
+    <h1 class="text-light mt-2">Welcome to aaron zhou space shooter game</h1>
+    <div class="h4 text-light mt-2 text-center">Once you download the zip, extract the contents and you will find the .exe and the assests folder. The .exe must be next to the assets folder for the game to work. Only for windows!</div>
+    <a href="aaron%20zhou%20game.zip" download="aaron zhou game.zip">Download</a>
+    <div class="h4 text-light mt-2 text-center">Have fUN!! this webpage will be deleted on 1/30/23 5 pm, download before it gets lost!</div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+<?php require_once './private/temp/footer.php'; ?>
+
