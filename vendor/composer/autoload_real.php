bf90275c09c1b9acaf8e729f26c021aefb09cfae
HypeByte <HypeByteProgrammer@gmail.com>
2023-06-20 04:05:42 -0700

    Setup mailing

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit1a2399b957c7d04fd854a3b9b1a45ad7
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit1a2399b957c7d04fd854a3b9b1a45ad7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit1a2399b957c7d04fd854a3b9b1a45ad7', 'loadClassLoader'));
